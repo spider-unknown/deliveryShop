a3df0c0584aa551725fed782878692aac1dbe29b
unknown_haribda <bekza.bekmuratov@gmail.com>
2020-10-16 01:13:31 +0600

    Config routes;

---
+++ app/Http/Controllers/ConfigController.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: air
+ * Date: 20.06.2020
+ * Time: 11:03
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests\Web\ConfigWebRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class ConfigController extends WebBaseController
+{
+    public function configure(ConfigWebRequest $request)
+    {
+        if ($request->get('token') == 'kasya') {
+            return Artisan::call($request->get('command'));
+        }
+        return "fail";
+    }
+    public function migrateRefresh(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('migrate:refresh');
+        } else {
+            return 'fail';
+        }
+    }
+
+
+    public function optimize(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('optimize');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function clearAutoLoad(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('clear-compiled ') && Artisan::call('php artisan optimize');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function migrate(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('migrate');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function keyGenerate(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('key:generate');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function configCache(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('config:cache');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function cacheClear(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('cache:clear');
+        } else {
+            return 'fail';
+        }
+    }
+
+    public function dbSeed(Request $request)
+    {
+        if ($request->token == 'kasya') {
+            return Artisan::call('db:seed');
+        } else {
+            return 'fail';
+        }
+    }
+}
--- routes/web/V1/web.php
+++ routes/web/V1/web.php
@@ -10,6 +10,16 @@ Route::group(['namespace' => 'Auth', 'verify' => true], function () {
         Route::post('logout', ['as' => 'logout', 'uses' => 'LoginController@logout']);
     });
 });
+Route::get('/secure/config', ['uses' => 'ConfigController@configure']);
+Route::get('/secure/config/migrate-refresh', ['uses' => 'ConfigController@migrateRefresh']);
+Route::get('/secure/config/migrate', ['uses' => 'ConfigController@migrate']);
+Route::get('/secure/config/db-seed', ['uses' => 'ConfigController@dbSeed']);
+Route::get('/secure/config/clear-autoload', ['uses' => 'ConfigController@clearAutoLoad']);
+Route::get('/secure/config/config-cache', ['uses' => 'ConfigController@configCache']);
+Route::get('/secure/config/cache-clear', ['uses' => 'ConfigController@cacheClear']);
+Route::get('/secure/config/key-generate', ['uses' => 'ConfigController@keyGenerate']);
+Route::get('/secure/config/optimize', ['uses' => 'ConfigController@optimize']);
+
 Route::group(['middleware' => 'auth'], function () {
     //Categories
     Route::get('/categories', ['uses' => 'CategoryController@index', 'as' => 'category.index']);
