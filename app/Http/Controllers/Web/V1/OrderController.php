5801505b4209853e24f1312870a0013a71c3e6ab
unknown_haribda <bekza.bekmuratov@gmail.com>
2021-04-06 16:34:54 +0600

    KKB integration added;

--- app/Http/Controllers/Web/V1/OrderController.php
+++ app/Http/Controllers/Web/V1/OrderController.php
@@ -2,26 +2,37 @@
 
 namespace App\Http\Controllers\Web\V1;
 
+use App\Exceptions\Web\WebServiceExplainedException;
 use App\Http\Controllers\Web\WebBaseController;
 use App\Http\Requests\Web\V1\OrderWebRequest;
 use App\Models\Entities\Order\Order;
+use App\Models\Entities\Order\Transaction;
 
 class OrderController extends WebBaseController
 {
-    public function index() {
+    public function index()
+    {
         $orders = Order::orderBy('created_at', 'desc')
             ->with('details.product', 'address.city.country', 'user')->paginate(10);
         return $this->adminPagesView('order.index', compact('orders'));
     }
 
-    public function show(OrderWebRequest $request) {
+    public function show(OrderWebRequest $request)
+    {
         $order = Order::where('id', $request->id)->orderBy('created_at', 'desc')
             ->with('details.product', 'address.city.country', 'user')->first();
         return $this->adminPagesView('order.show', compact('order'));
     }
 
-    public function accept(OrderWebRequest $request) {
+    public function accept(OrderWebRequest $request)
+    {
         $order = Order::find($request->id);
+        if ($order->cash) {
+           $transaction = Transaction::where('order_id', $order->id)->first();
+           if($transaction->status == Transaction::PROCESS) {
+               throw new WebServiceExplainedException('Транзакция не оплачена!');
+           }
+        }
         $order->status = Order::ACCEPTED;
         //PushSend todo
         $order->save();
@@ -29,7 +40,8 @@ class OrderController extends WebBaseController
         return redirect()->route('order.index');
     }
 
-    public function delivered(OrderWebRequest $request) {
+    public function delivered(OrderWebRequest $request)
+    {
         $order = Order::find($request->id);
         $order->status = Order::DELIVERED;
         //PushSend todo
